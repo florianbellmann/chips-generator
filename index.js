76bfabb70c2a7fb11591292336c915046ad4d03d
Florian Jüsten <florian.juesten@fjmbp.fritz.box>
2022-04-15 17:14:01 +0200

    changed to playwright

--- index.js
+++ index.js
@@ -1,48 +1,57 @@
-const dotenv = require("dotenv")
-const puppeteer = require('puppeteer')
-const fs = require("fs")
+const dotenv = require("dotenv");
+const playwright = require("playwright");
+const fs = require("fs");
 
 dotenv.config();
 
 (async () => {
+  const oldChips = parseInt(fs.readFileSync("chips", "utf8"));
+  console.log("Old Chips: ", oldChips);
 
-  const oldChips = parseInt(fs.readFileSync("chips", "utf8"))
-  console.log("Old Chips: ", oldChips)
-  
-  console.log("Launching browser and visitting site...")
-  const browser = await puppeteer.launch({ executablePath: 'chromium-browser' });
-  //const browser = await puppeteer.launch()
-  
-  const page = await browser.newPage()
-  await page.goto(process.env.website)
+  console.log("Launching browser and visitting site...");
+  const browser = await playwright.firefox.launch();
 
-  console.log("Logging in...")
-  await page.type('#login', process.env.username)
-  await page.type('#password', process.env.password)
-  await page.click('button[type=submit]')
+  const page = await (await browser.newContext()).newPage();
+  await page.goto(process.env.website);
 
-  // await page.waitForNavigation()
-  await delay(2000)
-  // console.log('New Page URL:', page.url())
-  console.log("Logging done...")
+  console.log("Logging in...");
+  await page.click("#onetrust-accept-btn-handler");
+  await page.waitForLoadState("networkidle"); // wait for the page to be loaded
 
-  // await page.screenshot({ path: 'screen.png' })
+  await page.type("#login", process.env.username);
+  await page.type("#password", process.env.password);
+  await page.click("button[type='submit']");
+  try {
+    await page.locator(".inApp-close").click();
+  } catch (error) {
+    console.log("no popup found");
+  }
+  await delay(4000);
 
-  const chipsDiv = await page.$("#header-user-chips")
-  const newChips = (await page.evaluate(chipsDiv => chipsDiv.textContent, chipsDiv)).trim().replace(",", "")
+  console.log("Logging done...");
+  const chipsDiv = await page.$("#header-user-chips");
+  const newChips = (
+    await page.evaluate((chipsDiv) => chipsDiv.textContent, chipsDiv)
+  )
+    .trim()
+    .replace(",", "");
 
-  console.log("New Chips: ", newChips)
-  fs.writeFileSync("chips", newChips)
-  fs.appendFile('run.log', `Script run at ${new Date()}. New Chips: ${newChips}.\r\n`, function (err) {
-    if (err) throw err;
-    console.log('Saved!');
-  });
+  console.log("New Chips: ", newChips);
+  fs.writeFileSync("chips", newChips);
+  fs.appendFile(
+    "run.log",
+    `Script run at ${new Date()}. New Chips: ${newChips}.\r\n`,
+    function (err) {
+      if (err) throw err;
+      console.log("Saved!");
+    }
+  );
 
-  await browser.close()
-})()
+  await browser.close();
+})();
 
 function delay(time) {
   return new Promise(function (resolve) {
-    setTimeout(resolve, time)
-  })
+    setTimeout(resolve, time);
+  });
 }
