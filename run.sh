f4ed3797f42b6193466548af6a5122aa2dbc3c95
florianbellmann <flojuesten@gmail.com>
2025-01-29 15:27:41 +0100

    run script

--- run.sh
+++ run.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-
 set -euo pipefail
 
 # Check if bun is installed
+#
+
+export $(cat ".env")
 
 if [ ! -f /usr/local/bin/bun ]; then
   echo "Bun is not installed. Installing bun..."
   curl -fsSL https://bun.sh/install | bash
 fi
 
-bun index.ts || bunx playwright install && bunx playwright install-deps && bun index
+bun index.ts >> app.log 2>&1 || bunx playwright install && bunx playwright install-deps && bun index.ts >> app.log 2>&1
 
 exit 0
